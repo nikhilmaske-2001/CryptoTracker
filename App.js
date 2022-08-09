d6e06adf11afff22aeda5237e456d95cf3c87e17
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-08-09 20:00:11 +0530

    Coin item container half done.

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'white' }}>Hello World</Text>
+      <View style={styles.coinContainer}>
+        <Image
+          source={{ uri: 'https://bitcoin.org/img/icons/opengraph.png?1657703267' }}
+          style={{ height: 30, width: 30, marginRight: 10, alignSelf: 'center' }}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.text}>1</Text>
+            <Text style={styles.text}>BTC</Text>
+            <Text style={styles.text}>0.63%</Text>
+          </View>
+        </View>
+        <View style={{ marginLeft: 'auto' }}>
+          <Text style={styles.title}>56268.90</Text>
+          <Text style={styles.text}>MCAP 1.085 T</Text>
+        </View>
+      </View>
       <StatusBar style="light" />
     </View>
   );
@@ -14,7 +31,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121212',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
   },
+  title: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'white',
+    marginRight: 5
+  },
+  coinContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'grey',
+    padding: 15,
+  }
 });
