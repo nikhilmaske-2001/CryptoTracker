9a41c0f605b83738eec0ef5376e6a61df921ac17
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-08-09 20:27:34 +0530

    Coin item component seperated from app.js

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet, View } from 'react-native';
+import CoinItem from './src/components/CoinItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.coinContainer}>
-        <Image
-          source={{ uri: 'https://bitcoin.org/img/icons/opengraph.png?1657703267' }}
-          style={{ height: 30, width: 30, marginRight: 10, alignSelf: 'center' }}
-        />
-        <View>
-          <Text style={styles.title}>Bitcoin</Text>
-          <View style={{ flexDirection: 'row' }}>
-            <View style={styles.rankContainer}>
-              <Text style={styles.rank}>1</Text>
-            </View>
-            <Text style={styles.text}>BTC</Text>
-            <AntDesign
-              name="caretdown" size={12} color="white"
-              style={{ alignSelf: 'center', marginRight: 5 }} />
-            <Text style={styles.text}>0.63%</Text>
-          </View>
-        </View>
-        <View style={{ marginLeft: 'auto' }}>
-          <Text style={styles.title}>56268.90</Text>
-          <Text style={styles.text}>MCAP 1.085 T</Text>
-        </View>
-      </View>
+      <CoinItem />
+      <CoinItem />
+      <CoinItem />
+      <CoinItem />
       <StatusBar style="light" />
     </View>
   );
@@ -38,31 +19,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#121212',
     paddingTop: 50,
-  },
-  title: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 3,
-  },
-  text: {
-    color: 'white',
-    marginRight: 5
-  },
-  coinContainer: {
-    flexDirection: 'row',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: 'grey',
-    padding: 15,
-  },
-  rank: {
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  rankContainer: {
-    backgroundColor: "#585858",
-    paddingHorizontal: 5,
-    borderRadius: 5,
-    marginRight: 5,
   }
 });
+
+
