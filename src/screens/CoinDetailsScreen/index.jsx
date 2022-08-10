eea6d0d71ca8070b247e75c12ed1b288de17c4e0
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-08-10 20:13:00 +0530

    Using sample example of react native chart js.

--- src/screens/CoinDetailsScreen/index.jsx
+++ src/screens/CoinDetailsScreen/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailHeader from "./components/CoinDetailedHeader";
 import styles from "./styles";
 import { AntDesign } from "@expo/vector-icons";
+import { LineChart } from "react-native-chart-kit";
 
 const CoinDetailedScreen = () => {
   const {
@@ -52,6 +53,48 @@ const CoinDetailedScreen = () => {
             {price_change_percentage_24h.toFixed(2)}%
           </Text>
         </View>
+      </View>
+      <View>
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width - 10} // from react-native
+          height={320}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
       </View>
     </View>
   );
