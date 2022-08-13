8363bf453d588850b5d686a52322c3dd5460eb7c
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-08-13 11:34:37 +0530

    Fetch more than 50 coins request implemented.

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from "react";
 import CoinItem from "../../components/CoinItem";
-import { ActivityIndicator, FlatList } from "react-native";
+import { ActivityIndicator, FlatList, RefreshControl } from "react-native";
 import { getMarketData } from "../../services/requests";
 
 const HomeScreen = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchCoins = async () => {
+  const fetchCoins = async (pageNumber) => {
+    // Avoid multiple api requests
+    if (loading) {
+      return;
+    }
     setLoading(true);
-    const fetchedCoins = await getMarketData();
-    setCoins(fetchedCoins);
+    const coinsData = await getMarketData(pageNumber);
+    setCoins((existingCoins) => [...existingCoins, ...coinsData]);
     setLoading(false);
   };
+
+  const refetchCoins = async () => {
+    // Avoid multiple api requests
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    const coinsData = await getMarketData();
+    setCoins(coinsData);
+    setLoading(false);
+  };
+
   useEffect(() => {
     fetchCoins();
   }, []);
@@ -25,6 +41,14 @@ const HomeScreen = () => {
     <FlatList
       data={coins}
       renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      onEndReached={() => fetchCoins(coins.length / 50 + 1)}
+      refreshControl={
+        <RefreshControl
+          refreshing={loading}
+          tintColor="white"
+          onRefresh={refetchCoins}
+        />
+      }
     />
   );
 };
--- src/services/requests.js
+++ src/services/requests.js
@@ -21,9 +21,9 @@ export const getCoinMarketChart = async (coinId) => {
 }
 
 // Homescreen Coin list
-export const getMarketData = async () => {
+export const getMarketData = async (pageNumber = 1) => {
     try {
-        const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=24h`);
+        const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${pageNumber}&sparkline=false&price_change_percentage=24h`);
         return response.data;
     } catch (error) {
         console.log(error);
