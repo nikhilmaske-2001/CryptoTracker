27319ee95b598a2e35d59122757b65feb5f4ce1c
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-08-13 10:52:18 +0530

    Loading coins list from api data.

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CoinItem from "../../components/CoinItem";
-import cryptocurrencies from "../../../assets/data/cryptocurrencies.json";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
+import { getMarketData } from "../../services/requests";
 
 const HomeScreen = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const fetchedCoins = await getMarketData();
+    setCoins(fetchedCoins);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" />;
+  }
+
   return (
     <FlatList
-      data={cryptocurrencies}
+      data={coins}
       renderItem={({ item }) => <CoinItem marketCoin={item} />}
     />
   );
--- src/services/requests.js
+++ src/services/requests.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Coin Detailed Screen Coin Details
 export const getDetailedCoinData = async (coinId) => {
     try {
         const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${coinId}?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false`);
@@ -9,9 +10,20 @@ export const getDetailedCoinData = async (coinId) => {
     }
 }
 
+// Coin Detailed Screen Chart
 export const getCoinMarketChart = async (coinId) => {
     try {
         const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=1&interval=hourly`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Homescreen Coin list
+export const getMarketData = async () => {
+    try {
+        const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=24h`);
         return response.data;
     } catch (error) {
         console.log(error);
