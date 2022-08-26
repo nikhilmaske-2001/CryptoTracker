81e91c49ae1dbabb762bf0f3a4cdb80d2fb028e1
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-08-26 11:49:25 +0530

    Using AsyncStorage to store coin watchlist data in the phone cache.

--- src/Contexts/WatchListContext.jsx
+++ src/Contexts/WatchListContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const WatchListContext = createContext();
 
@@ -6,6 +7,19 @@ export const useWatchList = () => useContext(WatchListContext);
 
 const WatchListProvider = ({ children }) => {
   const [watchListCoinIds, setwatchListCoinIds] = useState([]);
+
+  const getWatchListData = () => {
+    try {
+        const jsonValue = await AsyncStorage.getItem("@watchlist_coins");
+        setwatchListCoinIds(jsonValue != null ? JSON.parse(jsonValue): []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWatchListData();
+  }, []);
   return (
     <WatchListContext.Provider value={{ watchListCoinIds }}>
       {children}
