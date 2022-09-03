25b34694082f81355eb58a03a0879c854a8ceaff
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-09-03 11:14:08 +0530

    Wrote code for watchlistcoins data but it is not working.

--- src/screens/WatchListScreen/index.jsx
+++ src/screens/WatchListScreen/index.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList } from "react-native";
 import { useWatchList } from "../../Contexts/WatchListContext";
 import CoinItem from "../../components/CoinItem";
+import { getWatchlistCoins } from "../../services/requests";
 
 const WatchListScreen = () => {
   const { watchListCoinIds } = useWatchList();
+
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const transformCoinIds = () => watchListCoinIds.join("%2C");
+
+  const fetchWatchListedCoins = async () => {
+    if (loading) {
+      return true;
+    }
+    setLoading(true);
+    const watchlistCoinsData =
+      (await getWatchlistCoins(transformCoinIds())) || [];
+    setCoins([...coins, ...watchlistCoinsData]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchWatchListedCoins();
+  }, []);
+
+  // useEffect(() => {
+  //   refetchWatchListedCoins();
+  // });
+
   return (
     <View>
       <FlatList
-        data={watchListCoinIds}
+        data={coins}
         // renderItem={({ item }) => <CoinItem marketCoin={item} />}
       />
     </View>
--- src/services/requests.js
+++ src/services/requests.js
@@ -30,3 +30,14 @@ export const getMarketData = async (pageNumber = 1) => {
         console.log(error);
     }
 }
+
+// WatchlistedCoin data
+export const getWatchlistCoins = async (pageNumber = 1, coinIds) => {
+    try {
+        const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinIds}&order=market_cap_desc&per_page=100&page=${pageNumber}&sparkline=false`);
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
